47a187f256423d701f462f38e1d20b0a4f7fd71b
Max <sinner883@users.noreply.github.com>
2022-09-01 18:16:12 +0100

    Update index.js
    updated header

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="This website is underconstraction!" />
+        <Header title="This website is under construction!" />
         <p className="description">
           Get started by editing <code>pages/index.js</code>
         </p>
